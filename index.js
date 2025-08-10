841f3252bbe4437f01f7e70debef8323b80263a4
KaziMithfa <kazimithfa99@gmail.com>
2025-08-10 11:06:15 +0200

    server site setup for a single job

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -35,6 +35,15 @@ async function run() {
     // Get all jobs data from db
     app.get("/jobs", async (req, res) => {
       const result = await jobsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Get a single job data from db
+    app.get("/job/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const result = await jobsCollection.findOne(query);
       res.send(result);
     });
 
