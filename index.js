9d3fdce98363a673fd0ceb91d47aa8183116b57a
KaziMithfa <kazimithfa99@gmail.com>
2025-08-16 16:46:21 +0200

    Delete api has been created

--- index.js
+++ index.js
@@ -115,6 +115,16 @@ async function run() {
       res.send(result);
     });
 
+    // deleting a job by the user
+
+    app.delete("/job/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const result = await jobsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     //await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
