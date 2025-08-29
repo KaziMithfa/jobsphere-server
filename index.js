194e0d3e28daa4d52422085357f81b4f7d975ac3
KaziMithfa <kazimithfa99@gmail.com>
2025-08-29 13:10:20 +0200

    cookie has been created

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 require("dotenv").config();
@@ -34,6 +36,23 @@ async function run() {
     const appliedjobsCollection = client
       .db("jobSphere")
       .collection("appliedjobs");
+
+    // jwt generate
+
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "365d",
+      });
+
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.node_ENV === "production",
+          sameSite: process.env.node_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
 
     // Get all jobs data from db
     app.get("/jobs", async (req, res) => {
